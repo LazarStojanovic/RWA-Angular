629582383be9ceded6ebe2e843465f3519f14aab
Lazar Stojanovic <43703669+LazarStojanovic@users.noreply.github.com>
2019-09-07 21:45:57 +0200

    Ngrx store sredjivanje

--- src/app/store/post.action.ts
+++ src/app/store/post.action.ts
@@ -1,8 +1,15 @@
 import { Action } from "@ngrx/store";
+import { Post } from "../models/post";
 
-export const DELETE_POST = "[Delete post]"; //oivici se da se lepse formatira
+export const ADD_POST = "[Add post]";
+export const DELETE_POST = "[Delete post]";
 export const SELECT_POST = "[Select post]";
 
+export class AddPost implements Action {
+  readonly type = ADD_POST;
+
+  constructor(public posts: Post[]) {}
+}
 export class DeletePost implements Action {
   type = DELETE_POST;
   constructor(public postId: string) {}
--- src/app/store/posts.reducer.ts
+++ src/app/store/posts.reducer.ts
@@ -1,15 +1,19 @@
 import { Post } from "../models/post";
 import { Action } from "@ngrx/store";
-import { DELETE_POST, DeletePost } from "./post.action";
+import { ADD_POST, AddPost, DELETE_POST, DeletePost } from "./post.action";
 
 const initialState: Post[] = [
   {
-    id: "rambo",
+    id: "0",
     title: "Rambo"
   }
 ];
 export default function reducer(state: Post[] = initialState, action: Action) {
   switch (action.type) {
+    case ADD_POST:{
+      const { posts } = action as AddPost;
+      return [...state, ...posts];
+    }
     case DELETE_POST: {
       const { postId } = action as DeletePost;
       return state.filter((post: Post) => post.id !== postId);
