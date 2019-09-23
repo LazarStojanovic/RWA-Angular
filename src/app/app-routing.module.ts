43cc0f396d684f7a981d55c9f5e123710591804f
Lazar Stojanovic <43703669+LazarStojanovic@users.noreply.github.com>
2019-09-23 21:03:37 +0200

    Nesto kao sklopljeno zavrseno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './posts/components/home/home.component';
-import { AboutComponent } from './posts/components/about/about.component';
-import { RequestsComponent } from './posts/containers/requests/requests.component';
-import { RequestDetailComponent } from './posts/components/request-detail/request-detail.component';
-import { OffersComponent } from './posts/containers/offers/offers.component';
-import { OfferDetailComponent } from './posts/components/offer-detail/offer-detail.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./posts/components/home/home.component";
+import { AboutComponent } from "./posts/components/about/about.component";
+import { RequestsComponent } from "./posts/containers/requests/requests.component";
+import { RequestDetailComponent } from "./posts/components/request-detail/request-detail.component";
+import { OffersComponent } from "./posts/containers/offers/offers.component";
+import { OfferDetailComponent } from "./posts/components/offer-detail/offer-detail.component";
 
 const routes: Routes = [
-  {path: '',component: HomeComponent},
-  {path: 'about',component: AboutComponent},
-  { path: 'posts/requests',
-    component: RequestsComponent,
-    /*children:[
-      {path:':id', component: RequestDetailComponent}
-    ]*/
-  },
-  { path: 'posts/requests/:id', component: RequestDetailComponent},
-  { path: 'posts/offers',component: OffersComponent},
-  { path: 'posts/offers/:id', component: OfferDetailComponent}
+  { path: "", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "posts/requests", component: RequestsComponent },
+  { path: "posts/requests/:id", component: RequestDetailComponent },
+  { path: "posts/offers", component: OffersComponent },
+  { path: "posts/offers/:id", component: OfferDetailComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/posts/models/request.model.ts
+++ src/app/posts/models/request.model.ts
@@ -1,4 +1,5 @@
 export interface Request{
     id: number;
     title: string;
+    text:string;
 }
